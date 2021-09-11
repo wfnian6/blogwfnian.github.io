b7576daa446d3d3bc023ec6778ecb107a51e1c1a
wfnian6 <wfnian@gmail.com>
2021-09-11 10:27:21 +0800

    fuck again

---
+++ pictures.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import re
+import sys
+
+
+
+def format_url():
+    with open(sys.argv[1]) as f: 
+        data = str(f.readlines())
+        reg = re.compile('src="(https.*?)"',re.S)
+        res = reg.findall(string=data)
+    if len(res)!=0:
+        y,m,d,_ = sys.argv[1].split('/')[1].split('-')
+        div = """<div class="day">\n\t<div class="date">"""+y+'年'+m+'月'+d+"""日 | 北京市 </div>\n\t<div class="show">\n"""
+        pic_list=""
+        for item in res:
+            pic_list+='\t\t<div class="list"><img src="'+item+'"></div>\n'
+        div=div+pic_list+"\t</div>\n</div>\n"
+        print(div)
+
+format_url()
+ 
